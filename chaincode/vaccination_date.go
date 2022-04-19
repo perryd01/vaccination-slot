36fe23b66cc389e55759559330ead31fefbced5e
Ondrejó András <ondrejo.a@gmail.com>
2022-04-19 19:02:21 +0200

    Date format changed to 2006-01-02, added doc comments

--- chaincode/vaccination_date.go
+++ chaincode/vaccination_date.go
@@ -5,16 +5,19 @@ import (
 	"time"
 )
 
+// VaccinationDate is a simplified date format to identify specific occasions
 type VaccinationDate time.Time
 
+// MarshalJSON marshals the date into 2006-01-02 format
 func (vd *VaccinationDate) MarshalJSON() ([]byte, error) {
-	str := time.Time(*vd).Format("2006-01-02 15")
+	str := time.Time(*vd).Format("2006-01-02")
 	return []byte("\"" + str + "\""), nil
 }
 
+// UnmarshalJSON unmarshals date from 2006-01-02 format
 func (vd *VaccinationDate) UnmarshalJSON(b []byte) (err error) {
 	s := strings.Trim(string(b), `"`)
-	nt, err := time.Parse("2006-01-02 15", s)
+	nt, err := time.Parse("2006-01-02", s)
 	*vd = VaccinationDate(nt)
 	return
 }
