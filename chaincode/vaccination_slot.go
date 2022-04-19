401f81dadf9366cdcb4b1fc9437c9cf5a8b8bb8c
Ondrejó András <ondrejo.a@gmail.com>
2022-04-19 19:46:52 +0200

    Split token into multiple structs. Contract's ERC-721 interface is separated from other methods.

--- chaincode/vaccination_slot.go
+++ chaincode/vaccination_slot.go
@@ -1,12 +1,28 @@
+// Package chaincode implements ERC721 for HF9 Vaccination Slots.
 package chaincode
 
-type Token struct {
-	TokenId  string          `json:"tokenId"`
-	Type     string          `json:"type"`
-	Date     VaccinationDate `json:"date"`
-	Owner    string          `json:"owner"`
-	Burned   bool            `json:"approved"`
-	Approved string          `json:"approved"`
+// VaccinationSlotData contains information about specific occasion
+type VaccinationSlotData struct {
+	// Type of the vaccine.
+	// May change when the token is transferred.
+	Type string `json:"type"`
+
+	// When the vaccine should be administered.
+	// Never changes.
+	Date VaccinationDate `json:"date"`
+
+	// Previously administered vaccine of the same type.
+	// If present it may forbid the transfer of the token.
+	// May change when the token is transferred.
+	Previous string `json:"previous,omitempty"`
+}
+
+// VaccinationSlot contains ERC712 related data (this is the NFT)
+type VaccinationSlot struct {
+	VaccinationSlotData
+	TokenId  string `json:"tokenId"`
+	Owner    string `json:"owner"`
+	Approved string `json:"approved"`
 }
 
 type Approval struct {
