3a20e6c2dfd4568fad81e25040625da96ac2c88e
Dániel Miklós <daniel.miklos0@gmail.com>
2022-04-19 21:33:42 +0200

    fix cast

--- chaincode/vaccination_type.go
+++ chaincode/vaccination_type.go
@@ -2,36 +2,17 @@ package chaincode
 
 import (
 	"encoding/json"
-	"errors"
 )
 
 type VaccinationType string
 
 const (
-	Alpha   = "alpha"
-	Bravo   = "bravo"
-	Charlie = "charlie"
-	Delta   = "delta"
-	Echo    = "echo"
+	Alpha   VaccinationType = "alpha"
+	Bravo   VaccinationType = "bravo"
+	Charlie VaccinationType = "charlie"
+	Delta   VaccinationType = "delta"
+	Echo    VaccinationType = "echo"
 )
-
-func (vt *VaccinationType) FromString(s []byte) error {
-	switch string(s) {
-	case "alpha":
-		*vt = Alpha
-	case "bravo":
-		*vt = Bravo
-	case "charlie":
-		*vt = Charlie
-	case "delta":
-		*vt = Delta
-	case "echo":
-		*vt = Echo
-	default:
-		return errors.New("vaccinationType from string failed")
-	}
-	return nil
-}
 
 func (vt *VaccinationType) UnmarshalJSON(data []byte) error {
 	var s string
@@ -39,7 +20,7 @@ func (vt *VaccinationType) UnmarshalJSON(data []byte) error {
 	if err != nil {
 		return err
 	}
-	err = vt.FromString(data)
+	*vt = VaccinationType(s)
 	if err != nil {
 		return err
 	}
